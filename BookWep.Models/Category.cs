227bc97104cc58364f5b08930735501cd66bdd85
kyhung2002 <trankyhung225@gmail.com>
2023-05-13 23:19:39 +0700

    init category model

---
+++ BookWep.Models/Category.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWep.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+
+        public Category(int id, string name, int displayOrder)
+        {
+            Id = id;
+            Name = name;
+            DisplayOrder = displayOrder;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [Range(1, 100)]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
