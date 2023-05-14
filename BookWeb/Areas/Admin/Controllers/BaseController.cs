3da486efd00cad3443b6a7496e897522924873d2
kyhung2002 <trankyhung225@gmail.com>
2023-05-14 21:05:39 +0700

    Done Category Module

---
+++ BookWeb/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using BookWeb.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace BookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin)]
+    public class BaseController : Controller
+    {
+    }
+}
