f39213a723039980400e5b1c785feaa651cfa7b2
An <annogo123@gmail.com>
2023-05-17 10:17:18 +0700

    done user management module

---
+++ BookWeb.Models/ViewModels/UserVM.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.Models.ViewModels
+{
+    public class UserVM
+    {
+        [Required]
+        public string UserID { get; set; }
+        
+        [ValidateNever]
+        public ApplicationUser? ApplicationUser { get; set; }
+        
+        public string? RoleId { get; set; }
+        [ValidateNever]
+        //public IEnumerable<SelectListItem>? Roles { get; set; }
+        public IEnumerable<IdentityRole>? Roles { get; set; }
+
+        public string? CompanyId { get; set; }
+        [ValidateNever]
+        //public IEnumerable<SelectListItem>? Companies { get; set; }
+        public IEnumerable<Company>? Companies { get; set; }
+    }
+}
